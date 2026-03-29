f3ebfb529ab6beda87ae0d2d16de45a9c6aabaf3
Kenneth Ottas <kenneth.ottas@gmail.com>
2026-03-29 23:53:18 +0300

    13.1 constants.js 1

---
+++ constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+  
+    MAX_DRIVERS: 8,
+    MAX_NAME_LENGTH: 20,
+
+    RACE_MODES: {
+        DANGER: "DANGER",
+        SAFE: "SAFE",
+        FINISH: "FINISH"
+    },
+
+    TIMER: {
+        DEV: 60,
+        PROD: 600
+    }
+};
